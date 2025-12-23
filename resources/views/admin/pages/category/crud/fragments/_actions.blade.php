d8860643a7919ebc2de9f3eb68f4c39e9bd60138
unknown <Paris Ariyan>
2025-12-23 13:58:43 +0600

    added

--- resources/views/admin/pages/category/crud/fragments/_actions.blade.php
+++ resources/views/admin/pages/category/crud/fragments/_actions.blade.php
@@ -6,7 +6,7 @@
     @endcan
     @if(count($data['items']) > 0)
         @can('dyn_category_crud_bulk_update')
-            {{-- <button class="btn btn-success btn-soft-success waves-effect waves-light d-flex justify-items-center align-items-center  fw-bold  me-2 updateAllDynCategory"><span class="bx bx-save"></span> <span class="d-none d-md-block ms-1"> {{pxLang($data['lang'],'','common.btns.crud_update')}}   </span> </button> --}}
+            <button class="btn btn-success btn-soft-success waves-effect waves-light d-flex justify-items-center align-items-center  fw-bold  me-2 updateAllDynCategory"><span class="bx bx-save"></span> <span class="d-none d-md-block ms-1"> {{pxLang($data['lang'],'','common.btns.crud_update')}}   </span> </button>
         @endcan
         @can('dyn_category_crud_delete')
             <button class="btn btn-danger btn-soft-danger waves-effect waves-light waves-light d-flex justify-items-center align-items-center  fw-bold me-2 deleteAllDynCategory"><span class="bx bx-trash"></span> <span class="d-none d-md-block ms-1">  {{pxLang($data['lang'],'','common.btns.crud_delete')}}   </span> </button>
